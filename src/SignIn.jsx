32780085c7c901fb2611c64f5675314312d0ba76
Barikpega Nkabari <luckybarikpega80@gmail.com>
2026-07-24 12:46:44 +0100

    SignIn

--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -12,7 +12,7 @@ function SignIn() {
 
         {/* Heading */}
         <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-white">
-          Sign in to yourfffff account{" "}
+          Sign in to yourddddddddddfffff account{" "}
         </h2>
 
         {/* Form */}
