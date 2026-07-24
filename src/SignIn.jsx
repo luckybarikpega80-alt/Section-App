5de9725731aa81238e41b567e0f7d66eaaf96a3d
Barikpega Nkabari <luckybarikpega80@gmail.com>
2026-07-24 12:25:45 +0100

    Section App

--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -3,16 +3,16 @@ import { SiTailwindcss } from "react-icons/si";
 
 function SignIn() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-900 px-6">
+    <div className="min-h-screen flex items-center justify-center bg-purple-900 px-6">
       <div className="w-full max-w-md">
         {/* Logo */}
-        <div className="flex justify-center marrgin-top-10">
+        <div className="flex justify-center mt-10">
           <SiTailwindcss className="text-sky-400 text-5xl" />
         </div>
 
         {/* Heading */}
-        <h2 className="mt-10 text-center text-3xl font-bold tracking-tight text-white">
-          Sign in to your account
+        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-white">
+          Sign in to your account for login form{" "}
         </h2>
 
         {/* Form */}
@@ -30,7 +30,7 @@ function SignIn() {
               id="email"
               type="email"
               autoComplete="email"
-              className="mt-2 block w-full rounded-md border border-gray-700 bg-gray-800 px-3 py-2 text-white outline-none focus:border-indigo-500"
+              className="mt-2 block w-full rounded-md border border-white-700 bg-gray-800 px-3 py-2 text-white outline-none focus:border-indigo-500 placeholder:required:"
             />
           </div>
 
@@ -39,7 +39,7 @@ function SignIn() {
             <div className="flex items-center justify-between">
               <label
                 htmlFor="password"
-                className="block text-sm font-medium text-white"
+                className="block text-sm font-medium text-white placeholder:required:"
               >
                 Password
               </label>
@@ -56,7 +56,7 @@ function SignIn() {
               id="password"
               type="password"
               autoComplete="current-password"
-              className="mt-2 block w-full rounded-md border border-gray-700 bg-gray-800 px-3 py-2 text-white outline-none focus:border-indigo-500"
+              className="mt-2 block w-full rounded-md border border-white-700 bg-gray-800 px-3 py-2 text-white outline-none focus:border-indigo-500"
             />
           </div>
 
@@ -70,7 +70,7 @@ function SignIn() {
         </form>
 
         {/* Bottom Link */}
-        <p className="mt-10 text-center text-sm text-gray-400">
+        <p className="mt-10 text-center text-sm text-white">
           Not a member?{" "}
           <a
             href="#"
