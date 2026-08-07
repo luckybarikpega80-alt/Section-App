0c050fa2033890854600ccc3032838b62a974e70
Barikpega Nkabari <luckybarikpega80@gmail.com>
2026-08-07 11:42:38 +0100

    section app

--- src/Bentogrids.jsx
+++ src/Bentogrids.jsx
@@ -30,9 +30,12 @@ function Bentogrids() {
                 Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
                 lorem cupidatat commodo.
               </p>
+              <div className="border border-slate-700 h-151 mt-4 rounded-lg overflow-hidden">
+
+              </div>
             </div>
 
-            <img src={image1} alt="" className="w-full object-cover mt-4" />
+            {/* <img src={image1} alt="" className="w-full object-cover mt-4" /> */}
           </div>
 
           {/* PERFORMANCE */}
@@ -43,7 +46,7 @@ function Bentogrids() {
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
             </p>
 
-            <img src={image4} alt="" className="w-full mt-6" />
+            {/* <img src={image4} alt="" className="w-full mt-6" /> */}
           </div>
 
           {/* RIGHT CARD */}
@@ -56,14 +59,85 @@ function Bentogrids() {
                 eget.
               </p>
             </div>
+            <div className="bg-[#111827] rounded-xl overflow-hidden border border-slate-700">
+              {/* Tabs */}
+              <div className="flex border-b border-slate-700">
+                <div className="bg-slate-700 px-5 py-3 text-white text-sm font-medium border-r border-slate-600">
+                  NotificationSetting.jsx
+                </div>
 
-            <img src={image3} alt="" className="w-full mt-6" />
-            {/* <img src={image2} alt="" className="w-full object-cover mt-4" /> */}
+                <div className="bg-[#111827] px-5 py-3 text-slate-400 text-sm font-medium">
+                  App.jsx
+                </div>
+              </div>
+
+              {/* Fake Code */}
+              <pre className="p-6 text-sm leading-7 overflow-hidden">
+                <span className="text-blue-400">import</span>{" "}
+                <span className="text-white">{"{ useState }"}</span>{" "}
+                <span className="text-blue-400">from</span>{" "}
+                <span className="text-green-400">"react"</span>
+                {"\n"}
+                <span className="text-blue-400">import</span>{" "}
+                <span className="text-white">{"{ Switch }"}</span>{" "}
+                <span className="text-blue-400">from</span>{" "}
+                <span className="text-green-400">"@headlessui/react"</span>
+                {"\n\n"}
+                <span className="text-blue-400">function</span>{" "}
+                <span className="text-indigo-400">Example</span>() {"{"}
+                {"\n"}
+                {"  "}
+                <span className="text-blue-400">const</span>{" "}
+                <span className="text-white">[enabled, setEnabled]</span> =
+                <span className="text-indigo-400"> useState</span>
+                <span className="text-white">(true)</span>
+                {"\n\n"}
+                {"  "}
+                <span className="text-blue-400">return</span> ({"\n"}
+                {"    "}
+                <span className="text-gray-300">{"<form action="}</span>
+                <span className="text-green-400">"/notification-settings"</span>
+                <span className="text-gray-300">{" method="}</span>
+                <span className="text-green-400">"post"</span>
+                <span className="text-gray-300">{">"}</span>
+                {"\n"}
+                {"      "}
+                <span className="text-sky-400">{"<Switch"}</span>
+                {"\n"}
+                {"        "}
+                <span className="text-yellow-300">checked</span>=
+                <span className="text-white">{"{enabled}"}</span>
+                {"\n"}
+                {"        "}
+                <span className="text-yellow-300">onChange</span>=
+                <span className="text-white">{"{setEnabled}"}</span>
+                {"\n"}
+                {"      "}
+                <span className="text-sky-400">{">"}</span>
+                {"\n"}
+                {"        "}
+                <span className="text-gray-500">{"{/* ... */}"}</span>
+                {"\n"}
+                {"      "}
+                <span className="text-sky-400">{"</Switch>"}</span>
+                {"\n"}
+                {"      "}
+                <span className="text-sky-400">{"<button>"}</span>
+                Submit
+                <span className="text-sky-400">{"</button>"}</span>
+                {"\n"}
+                {"    "}
+                <span className="text-gray-300">{"</form>"}</span>
+                {"\n"}
+                {"  "}){"\n"}
+                {"}"}
+              </pre>
+            </div>
           </div>
 
           {/* SECURITY */}
           <div className="bg-[#1E293B] h-90 rounded-2xl border border-slate-700 p-6">
-            <>
+            <div className="flex flex-col items-center justify-center h-full">
               {/* Text */}
               <h2 className="text-white text-2xl font-bold">Security</h2>
 
@@ -73,7 +147,7 @@ function Bentogrids() {
               </p>
 
               {/* Icons */}
-              <div className="flex items-center justify-between mt-10">
+              <div className="flex items-center justify-between gap-6 mt-6 w-full max-w-xs">
                 {/* Cloud */}
                 <div className="w-16 h-16 rounded-full bg-slate-600 flex items-center justify-center">
                   <Cloud className="text-white w-7 h-7" />
@@ -96,7 +170,7 @@ function Bentogrids() {
                   <Database className="text-white w-7 h-7" />
                 </div>
               </div>
-            </>
+            </div>
           </div>
         </div>
       </div>
--- src/Home.jsx
+++ src/Home.jsx
@@ -2,59 +2,97 @@ import { Link } from "react-router";
 
 const Home = () => {
   return (
-    <section className="min-h-screen bg-[#0F172A] flex flex-col items-center py-10">
-      <h1 className="text-5xl font-bold text-white mb-10">MY WEBSITE PAGE</h1>
+    <section className="min-h-screen bg-[#0E1628] flex flex-col items-center py-10">
+      {/* Heading */}
+      <div className="text-center mb-12">
+        <h1 className="text-8xl font-black text-purple-700 leading-none">
+          LUSTER JNR
+        </h1>
 
-      <div className="w-full max-w-3xl flex flex-col items-center justify-center gap-8">
+        <h2 className="text-6xl font-black text-purple-700 leading-none mt-2">
+          TECH AND DESIGN
+        </h2>
+
+        <h3 className="text-4xl font-extrabold tracking-[10px] text-purple-600 mt-3">
+          SECTION PAGE
+        </h3>
+      </div>
+
+      {/* Grid */}
+      <div className="grid grid-cols-6 gap-5">
         <Link
           to="/signin"
-          className="bg-transparent border border-white text-white h-13 w-70 rounded-3xl   flex items-center justify-center text-4xl font-bold"
+          className="w-52 h-40 bg-amber-400 rounded-3xl flex items-center justify-center text-3xl font-bold text-[#0E1628] hover:scale-105 transition"
         >
           SIGN IN 1
         </Link>
 
         <Link
           to="/signin2"
-          className="bg-transparent border border-white text-white h-13 w-70 rounded-3xl  flex items-center justify-center text-4xl font-bold"
+          className="w-52 h-40 bg-amber-400 rounded-3xl flex items-center justify-center text-3xl font-bold text-[#0E1628] hover:scale-105 transition"
         >
           SIGN IN 2
         </Link>
 
         <Link
           to="/hero"
-          className="bg-transparent border border-white text-white h-13 w-70 rounded-3xl flex items-center justify-center text-4xl font-bold"
+          className="w-52 h-40 bg-amber-400 rounded-3xl flex items-center justify-center text-3xl font-bold text-[#0E1628] hover:scale-105 transition"
         >
           HERO 1
         </Link>
 
         <Link
           to="/hero2"
-          className="bg-transparent border border-white text-white h-13 w-70 rounded-3xl  flex items-center justify-center text-4xl font-bold"
+          className="w-52 h-40 bg-amber-400 rounded-3xl flex items-center justify-center text-3xl font-bold text-[#0E1628] hover:scale-105 transition"
         >
           HERO 2
         </Link>
 
         <Link
           to="/cta"
-          className="bg-transparent border border-white text-white h-13 w-70 rounded-3xl  flex items-center justify-center text-4xl font-bold"
+          className="w-52 h-40 bg-amber-400 rounded-3xl flex items-center justify-center text-3xl font-bold text-[#0E1628] hover:scale-105 transition"
         >
-          CTA SECTION 1
+          CTA 1
         </Link>
 
         <Link
           to="/cta2"
-          className="bg-transparent border border-white text-white h-13 w-76 rounded-3xl  flex items-center justify-center text-4xl font-bold"
+          className="w-52 h-40 bg-amber-400 rounded-3xl flex items-center justify-center text-3xl font-bold text-[#0E1628] hover:scale-105 transition"
         >
-          CTA SECTION 2
+          CTA 2
         </Link>
 
         <Link
           to="/bentogrids"
-          className="bg-transparent border border-white text-white h-13 w-76 rounded-3xl flex items-center justify-center text-4xl font-bold"
+          className="w-52 h-40 bg-amber-400 rounded-3xl flex items-center justify-center text-3xl font-bold text-[#0E1628] hover:scale-105 transition"
         >
-          BENTO GRID
+          BENTO
         </Link>
+
+        <Link
+          to="/app"
+          className="w-52 h-40 bg-amber-400 rounded-3xl flex items-center justify-center text-3xl font-bold text-[#0E1628] hover:scale-105 transition"
+        >
+          APP
+        </Link>
+
+        {/* Empty Boxes */}
+        <div className="w-52 h-40 bg-amber-400 rounded-3xl"></div>
+        <div className="w-52 h-40 bg-amber-400 rounded-3xl"></div>
+        <div className="w-52 h-40 bg-amber-400 rounded-3xl"></div>
+        <div className="w-52 h-40 bg-amber-400 rounded-3xl"></div>
+        <div className="w-52 h-40 bg-amber-400 rounded-3xl"></div>
+        <div className="w-52 h-40 bg-amber-400 rounded-3xl"></div>
+        <div className="w-52 h-40 bg-amber-400 rounded-3xl"></div>
+        <div className="w-52 h-40 bg-amber-400 rounded-3xl"></div>
+        <div className="w-52 h-40 bg-amber-400 rounded-3xl"></div>
+        <div className="w-52 h-40 bg-amber-400 rounded-3xl"></div>
       </div>
+
+      {/* Footer */}
+      <p className="text-white italic text-4xl mt-16">
+        copy right. Alright right reserve luster jnr tech and design 2026
+      </p>
     </section>
   );
 };
